5608cd4803f2406f4f4a874c949323d0f65696ac
Jue0 <51354931+Jue0@users.noreply.github.com>
2022-07-04 17:05:31 +0900

    Update nuscenes_targz_manager.py

--- nuscenes_targz_manager.py
+++ nuscenes_targz_manager.py
@@ -21,34 +21,26 @@ output: Files extracted from compressed files,
 ## 라이브러리
 import argparse
 from ast import arg
+from email.utils import localtime
 import logging
 import tarfile
+import time
 
 import os
 import glob
-import csv
+from tracemalloc import start
 
-
-
-if __name__=='__main__':
-    ju=0
-
-    logger = logging.getLogger()
-
-    parser = argparse.ArgumentParser(description='인자값 받기')
-    parser.add_argument('--src', required=True, help='압축파일들 있는 경로')    
-    parser.add_argument('--dst', required=True, help='압축해제할 경로')    
-
-    args = parser.parse_args()
-    logger.info(args.src)
-    logger.info(args.dst)
-    
-    filelist = glob.glob(args.src+"/*.tar") + glob.glob(args.src+"/*.tgz")
+def write_output(filelist):
     # print(filelist)
     # logger.info(filelist)
 
     ## output 파일 열고, 파일 이름 로깅
-    with open(args.src+'/output.txt', 'a') as output:
+    start = time.time()
+
+    # output_filename = '/' + time.strftime('%Y-%m-%d_%I-%M-%S', localtime(start)) + '.txt'
+    # output_filename = '/output.txt'
+    # with open(os.path.join(args.src, output_filename), 'a') as output:
+    with open(args.src + '/output.txt', 'a') as output:
         for f in filelist:
             if f.split('.')[1]=='tgz':
                 # print(f.split('.')[1])
@@ -70,5 +62,38 @@ if __name__=='__main__':
             output.write(l+ '\n\n')
             
             file.close()
-            
+    
+    end = time.time()
+    print('time elapsed(s): ', round(end - start, 2))
+
+def extract_tar(filelist):
     ## 압축파일 해제
+    for f in filelist:
+        if f.split('.')[1]=='tgz':
+            # print(f.split('.')[1])
+            logger.info(f.split('.')[1])
+            file = tarfile.open(f, 'r:gz')
+        else:
+            # print(f.split('.')[1])
+            logger.info(f.split('.')[1])
+            file = tarfile.open(f, 'r')
+
+        file.extractall(path=args.dst)
+        file.close()
+
+
+if __name__=='__main__':
+    logger = logging.getLogger()
+
+    parser = argparse.ArgumentParser(description='인자값 받기')
+    parser.add_argument('--src', required=True, help='압축파일들 있는 경로')    
+    parser.add_argument('--dst', required=True, help='압축해제할 경로')    
+
+    args = parser.parse_args()
+    logger.info(args.src)
+    logger.info(args.dst)
+    
+    filelist = glob.glob(args.src+"/*.tar") + glob.glob(args.src+"/*.tgz")
+    
+    write_output(filelist)
+    # extract_tar(filelist)
